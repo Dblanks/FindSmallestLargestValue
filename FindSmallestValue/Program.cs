43b5e8d9496fadd9bb07ce07591bce6d1e85cac6
Dom Blanks <dblanks948@gmail.com>
2019-06-11 22:18:45 -0500

    FindSmallestLargest value in a given set of numbers

--- FindSmallestValue/Program.cs
+++ FindSmallestValue/Program.cs
@@ -3,19 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Simple console application to find the smallest and largest number of a given set
+ */
 namespace FindSmallestValue
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //declare variables 
+            int index = 10;
+            int smallest = 0;
+            int largest = 0;
+            int inputNum = 0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            for(int i = 0; i <= index; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                inputNum = Int32.Parse(Console.ReadLine());
+
+                //check for the first number assume it is the smallest/largest
+                //else check if the number input is smaller/larger the previous
+                if(i == 0)
+                {
+                    smallest = inputNum;
+                    largest = inputNum;
+                }else if(inputNum < smallest)
+                {
+                    smallest = inputNum;
+                }else if(inputNum > largest)
+                {
+                    largest = inputNum;
+                }
+            }
+
+            Console.WriteLine(" Smallest: {0} \n Largest:{1}", smallest, largest);
+            Console.Read();
+
+            
+
+
+
         }
     }
 }
